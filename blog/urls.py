3cd123d9a7bba8bea1768f236a22174fe50e8169
Gamal-prog <atemil791@gmail.com>
2023-10-17 22:23:48 +0600

    Third Commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostList
+from .views import PostList, PostDetail, PostNew
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('post/new/', PostNew.as_view(), name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 class PostList(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostNew(CreateView):
+    form_class = PostForm
+    template_name = 'blog/post_new.html'
+    Permission_required = ('new.add_post')
+    raise_exception = True
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+        
 
