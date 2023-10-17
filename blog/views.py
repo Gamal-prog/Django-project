f3e29ba5718a2ce0002e4ad4aa9ca47339797142
Gamal-prog <atemil791@gmail.com>
2023-10-17 21:36:20 +0600

    Second commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+
